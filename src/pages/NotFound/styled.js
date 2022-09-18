0de81ee6e1759430aa11a7e1ce25c66565a1a92b
NatyCG <naty.l.araujo2@gmail.com>
2022-09-18 18:58:31 -0300

    Menu básico- Teste de login- Página 404

---
+++ src/pages/NotFound/styled.js
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+export const NotFoundArea = styled.div`
+
+background-color: #4B0082;
+
+.container{
+    max-width:1200px;
+    margin: auto 0;
+    align-items: center;
+    justify-content: center;
+    display: flex;
+    height: 400px;
+    flex-direction: column;
+
+    h2{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+.botao{
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    
+
+    a{
+        text-decoration: none;
+        padding: 10px 20px;
+        border: 1px solid #DDA0DD;
+        display: flex;
+        color:#DCDCDC;
+    }
+}
+}
+`
